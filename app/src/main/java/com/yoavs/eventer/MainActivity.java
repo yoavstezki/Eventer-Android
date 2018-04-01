cf1457ba5bc0c1cda40e7cd176b496187f76052f
Yoav Stezki <yoavs@pinkpark.com>
2018-04-01 14:09:37 +0300

    Add Login with facebook.

--- app/src/main/java/com/yoavs/eventer/MainActivity.java
+++ app/src/main/java/com/yoavs/eventer/MainActivity.java
@@ -1,13 +1,57 @@
 package com.yoavs.eventer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    LoginButton loginButton;
+    TextView textView;
+    CallbackManager callbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = findViewById(R.id.fb_login_bn);
+        textView = findViewById(R.id.textView);
+
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                textView.setText("Login Success \n" +
+                        loginResult.getAccessToken().getUserId() + "\n" +
+                        loginResult.getAccessToken().getToken());
+            }
+
+            @Override
+            public void onCancel() {
+
+                textView.setText("Login Cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
